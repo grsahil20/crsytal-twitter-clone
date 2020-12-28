4e52a371c82862be0b82270c4fdb5434986a4b45
Sahil Grover <gr.sahil20@gmail.com>
2020-12-28 20:12:43 +0100

    cleanup

--- src/operations/save_user.cr
+++ src/operations/save_user.cr
@@ -1,13 +1,13 @@
 class SaveUser < User::SaveOperation
-  permit_columns email
+  permit_columns email, name
 
-  before_save do
-    validate_required email
-  end
+  # before_save do
+  #   validate_required email
+  # end
 
-  before_save reject_scary_monsters
+  # before_save reject_scary_monsters
 
-  def reject_scary_monsters
-    email.add_error "Mmmyyaahhh!" if email.value == "ich.heisse.sahil@gmail.comm"
-  end
+  # def reject_scary_monsters
+  #   email.add_error "Mmmyyaahhh!" if email.value == "ich.heisse.sahil@gmail.comm"
+  # end
 end
